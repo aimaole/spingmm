b1c0dadc7a1d88fc59c5c094e83ae8944ee80c57
337340028 <337340028@qq.com>
2020-05-26 08:43:11 +0800

    first commit

--- src/test/java/com/mm/springmm/SpringmmApplicationTests.java
+++ src/test/java/com/mm/springmm/SpringmmApplicationTests.java
@@ -1,9 +1,12 @@
 package com.mm.springmm;
 
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +24,22 @@ class SpringmmApplicationTests {
         CreateIndexRequest request = new CreateIndexRequest("test1");
         CreateIndexResponse createIndexResponse = restHighLevelClient.indices().create(request, RequestOptions.DEFAULT);
         System.out.println(createIndexResponse);
+
+    }
+
+    @Test
+    void testExitIndex() throws IOException {
+        GetIndexRequest getIndexRequest = new GetIndexRequest("test1");
+        boolean exists = restHighLevelClient.indices().exists(getIndexRequest, RequestOptions.DEFAULT);
+        System.out.println(exists);
+
+    }
+
+    @Test
+    void testDelIndex() throws IOException {
+        DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest("test1");
+        AcknowledgedResponse delete = restHighLevelClient.indices().delete(deleteIndexRequest, RequestOptions.DEFAULT);
+        System.out.println(delete.isAcknowledged());
 
     }
 
